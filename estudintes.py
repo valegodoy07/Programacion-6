bb907b9907a19dc7b2d76597af01951290fc8f7b
valentina godoy <valentinagodoy0267@gmail.com>
2025-05-19 09:45:31 -0300

    Update estudintes.py

--- estudintes.py
+++ estudintes.py
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
 --
--- Host: localhost    Database: araceli
+-- Host: localhost    Database: valentina
 -- ------------------------------------------------------
 -- Server version	8.0.29
 
